4b5877ec2c6c10ff0a4567be8628d2743f2085be
suryascodeverses <chauhanaman1912@gmail.com>
2025-04-04 19:59:10 +0530

    initial setup

---
+++ src/data/sidebar-menus.ts
@@ -0,0 +1,95 @@
+import { ISidebarMenus } from "./../types/menu-types";
+import {
+  Dashboard,
+  Categories,
+  Coupons,
+  Orders,
+  Pages,
+  Products,
+  Profile,
+  Reviews,
+  Setting,
+  Leaf,
+  StuffUser,
+} from "@/svg";
+
+const sidebar_menu: Array<ISidebarMenus> = [
+  {
+    id: 1,
+    icon: Dashboard,
+    link: "/dashboard",
+    title: "Dashboard",
+  },
+  {
+    id: 2,
+    icon: Products,
+    link: "/product-list",
+    title: "Products",
+    subMenus: [
+      { title: "Product List", link: "/product-list" },
+      { title: "Product Grid", link: "/product-grid" },
+      { title: "Add Product", link: "/add-product" }
+    ],
+  },
+  {
+    id: 3,
+    icon: Categories,
+    link: "/category",
+    title: "Category",
+  },
+  {
+    id: 4,
+    icon: Orders,
+    link: "/orders",
+    title: "Orders",
+  },
+  {
+    id: 5,
+    icon: Leaf,
+    link: "/brands",
+    title: "Brand",
+  },
+  {
+    id: 6,
+    icon: Reviews,
+    link: "/reviews",
+    title: "Reviews",
+  },
+  {
+    id: 7,
+    icon: Coupons,
+    link: "/coupon",
+    title: "Coupons",
+  },
+  {
+    id: 8,
+    icon: Profile,
+    link: "/profile",
+    title: "Profile",
+  },
+  {
+    id: 9,
+    icon: Setting,
+    link: "#",
+    title: "Online store",
+  },
+  {
+    id: 10,
+    icon: StuffUser,
+    link: "/our-staff",
+    title: "Our Staff",
+  },
+  {
+    id: 11,
+    icon: Pages,
+    link: "/dashboard",
+    title: "Pages",
+    subMenus: [
+      { title: "Register", link: "/register" },
+      { title: "Login", link: "/login" },
+      { title: "Forgot Password", link: "/forgot-password" }
+    ],
+  },
+];
+
+export default sidebar_menu;
